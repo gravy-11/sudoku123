291d530147d809f6b664f3b50e2cac3fdc28a199
gravy-11 <gravy667dev@gmail.com>
2022-03-07 21:40:31 +0900

    feat: pencilmarkの仮表示実装

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,43 +1,82 @@
 import { Cell, X } from "./Cell";
 
 export class Board {
-  constructor(private cells: Cell[][]) {}
+  constructor(private cells: Cell[]) {}
 
   static from(input: string) {
     if (!/^[0-9]{81}$/.test(input)) {
       throw new RangeError("Input must be 81-digit string");
     }
 
-    const cells = [] as Cell[][];
-    input.split("").forEach((s, index) => {
+    const cells = input.split("").map((s, index) => {
       const row = Math.floor(index / 9);
       const col = index % 9;
-      if (col === 0) {
-        cells.push([new Cell(+s as X)]);
-        return;
-      }
-      cells[row].push(new Cell(+s as X));
+      return Cell.create([row, col], +s as X);
     });
     return new Board(cells);
   }
 
-  toString() {
+  toString(pencilMarks = false) {
     let result = "";
     for (let row = 0; row < 9; row++) {
-      if (row === 0) result += ".-----.-----.-----.\n";
-      if (row === 3 || row === 6) result += ":----- ----- -----:\n";
-      result += this.getRowString(row);
+      if (row === 0) result += this.getTopBorder(pencilMarks);
+      if (row === 3 || row === 6) result += this.getSeparator(pencilMarks);
+      result += this.getRowString(row, pencilMarks);
     }
-    result += ".-----.-----.-----.\n";
+    result += this.getBottomBorder(pencilMarks);
     return result;
   }
 
-  private getRowString(row: number) {
+  private getCell(row: number, col: number) {
+    return this.cells[row * 9 + col];
+  }
+
+  private getRowCells(row: number) {
+    return this.cells.filter((cell) => cell.getRow() === row);
+  }
+
+  private getColCells(col: number) {
+    return this.cells.filter((cell) => col === cell.getCol());
+  }
+
+  private getBoxCells(row: number, col: number) {
+    const target = this.getCell(row, col);
+    return this.cells.filter((cell) => cell.isInSameBox(target));
+  }
+
+  private getRowString(row: number, pencilMarks = false) {
     let result = "";
     for (let col = 0; col < 9; col++) {
-      result += (col % 3 === 0 ? "|" : " ") + this.cells[row][col].toString();
+      if (pencilMarks) {
+        const preposition = col % 3 === 0 ? "| " : " ";
+        result += preposition + this.getCell(row, col).toString(pencilMarks);
+      } else {
+        const preposition = col % 3 === 0 ? "|" : " ";
+        result += preposition + this.getCell(row, col).toString();
+      }
     }
     result += "|\n";
     return result;
+  }
+
+  private getTopBorder(pencilMarks = false) {
+    return this.getLine(".", ".", pencilMarks);
+  }
+
+  private getSeparator(pencilMarks = false) {
+    return this.getLine(":", " ", pencilMarks);
+  }
+
+  private getBottomBorder(pencilMarks = false) {
+    return this.getLine("'", "'", pencilMarks);
+  }
+
+  private getLine(end: string, middle: string, pencilMarks = false) {
+    let barCount = 5;
+    if (pencilMarks) {
+      barCount = 24;
+    }
+    const line = "-".repeat(barCount);
+    return end + line + middle + line + middle + line + end + "\n";
   }
 }
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,9 +1,70 @@
 export type X = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type Position = [number, number];
 
 export class Cell {
-  constructor(private value: X) {}
+  constructor(private position: Position, protected value: X) {}
 
-  toString() {
+  static create(position: Position, value: X) {
+    if (value === 0) {
+      return new WritableCell(position, value);
+    }
+    return new Cell(position, value);
+  }
+
+  toString(pencilMarks = false) {
+    if (pencilMarks) {
+      return this.value + " ".repeat(6);
+    }
     return this.value === 0 ? "." : "" + this.value;
+  }
+
+  getRow() {
+    return this.position[0];
+  }
+
+  getCol() {
+    return this.position[1];
+  }
+
+  getBox() {
+    return this.position.map((x) => Math.floor(x / 3)) as [number, number];
+  }
+
+  isInSameBox(cell: Cell) {
+    const [row1, col1] = this.getBox();
+    const [row2, col2] = cell.getBox();
+    return row1 === row2 && col1 === col2;
+  }
+
+  isWritable() {
+    return this instanceof WritableCell;
+  }
+}
+
+export class WritableCell extends Cell {
+  candidates: Set<X>;
+
+  constructor(position: Position, value: X) {
+    super(position, value);
+    this.candidates = new Set();
+  }
+
+  toString(pencilMarks = false): string {
+    if (pencilMarks) {
+      return (" ".repeat(7) + this.getCandidates().join("")).slice(-7);
+    }
+    return super.toString();
+  }
+
+  addCandidate(value: X) {
+    this.candidates.add(value);
+  }
+
+  getCandidates() {
+    return Array.from(this.candidates).sort();
+  }
+
+  removeCandidate(value: X) {
+    this.candidates.delete(value);
   }
 }
